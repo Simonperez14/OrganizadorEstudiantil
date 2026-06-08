be3fde64ac8446ade1c5ff3cb2376e592f5089c5
Simonperez14 <simonpc10911@gmail.com>
2026-06-08 02:47:50 -0300

    Config de CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import rutasGenerales from "./server/v1/routes/index.js"
+import cors from "cors";
 import { rutaNoEncontradaMiddleware } from "./server/v1/middleware/ruta-no-encontrada.Middleware.js";
 import xssSanitizer from "./server/v1/middleware/sanitizer-middleware.mjs";
 //import dotenv from "dotenv";
@@ -10,9 +11,21 @@ import { errorMiddleware } from "./server/v1/middleware/error.middleware.js";
 
 connectMongo();
 
-
-
 const app = express();
+
+const corsOptions = {
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:3001",
+        "http://localhost:3002",
+        "https://organizador-estudiantil.vercel.app/",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true, // solo si usás cookies/sesión
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
